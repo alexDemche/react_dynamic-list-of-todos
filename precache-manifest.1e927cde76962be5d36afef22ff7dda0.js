fe59d2cc6d278ff735f4f228243fca72eeb4750a
alexDemche <alexeyydemchenko@gmail.com>
2019-09-23 16:18:37 +0300

    Updates

---
+++ precache-manifest.1e927cde76962be5d36afef22ff7dda0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5231477a1539f7ca25964cc4896f3bbe",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "af8bd3656c43cddc62e5",
+    "url": "/react_dynamic-list-of-todos/static/css/main.102bd186.chunk.css"
+  },
+  {
+    "revision": "f3f1ff399754e15cef8f",
+    "url": "/react_dynamic-list-of-todos/static/js/2.544d08e0.chunk.js"
+  },
+  {
+    "revision": "af8bd3656c43cddc62e5",
+    "url": "/react_dynamic-list-of-todos/static/js/main.10bd680f.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
